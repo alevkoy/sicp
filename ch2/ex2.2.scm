96809321b5eb13ce391804422b9e23da705bbd7c
Abraham Levkoy <alevkoy@users.noreply.github.com>
2016-08-08 23:13:52 -0400

    Complete exercise 2.3

--- ch2/ex2.2.scm
+++ ch2/ex2.2.scm
@@ -56,3 +56,85 @@
                         (make-point 10 6)))
 (print-segment s)
 (print-point (midpoint-segment s))
+
+; Exercise 2.3
+; square adapted from SICP.
+
+; Implementation #1: 3 points (The 4th point could be derived if necessary.)
+(define (make-rectangle p1 p2 p3)
+    (cons p1 (cons p2 p3)))
+
+(define (p1-rectangle r)
+    (car r))
+
+(define (p2-rectangle r)
+    (car (cdr r)))
+
+(define (p3-rectangle r)
+    (car (cdr r)))
+
+(define (square x) (* x x))
+
+(define (length-segment s)
+    ; Use Pythagorean Theorem
+    (let ((x-dist (- (x-point (end-point s))
+                     (x-point (start-point s))))
+          (y-dist (- (y-point (end-point s))
+                     (y-point (start-point s)))))
+        (sqrt (+ (square x-dist) (square y-dist)))))
+
+; Arbitrarily, the p1->p2 distance is height
+(define (height-rectangle r)
+    (length-segment (make-segment (p1-rectangle r)
+                                  (p2-rectangle r))))
+
+; Since p1->p2 is height, p2->p3 must be width
+(define (width-rectangle r)
+    (length-segment (make-segment (p1-rectangle r)
+                                  (p2-rectangle r))))
+
+(define (area-rectangle r)
+    (* (width-rectangle r) (height-rectangle r)))
+
+(define (perimeter-rectangle r)
+    (* (+ (width-rectangle r) (height-rectangle r))))
+
+; A rectangle to use for testing
+(define r (make-rectangle (make-point 0 1)
+                          (make-point 2 3)
+                          (make-point 4 1)))
+
+(define (test-display r)
+    (newline)
+    (display "Area: ")
+    (display (area-rectangle r))
+    (newline)
+    (display "Perimeter: ")
+    (display (perimeter-rectangle r)))
+
+(test-display r)
+
+; Implementation #2: 2 segments
+; s1 and s2 are intersecting line segments in a rectangle. They are assumed to
+; share a point.
+(define (make-rectangle s1 s2)
+    (cons s1 s2))
+
+(define (s1-rectangle r)
+    (car r))
+
+(define (s2-rectangle r)
+    (cdr r))
+
+(define (height-rectangle r)
+    (length-segment (s1-rectangle r)))
+
+(define (width-rectangle r)
+    (length-segment (s2-rectangle r)))
+
+(define r (make-rectangle (make-segment (make-point 0 1)
+                                        (make-point 2 3))
+                          (make-segment (make-point 2 3)
+                                        (make-point 4 1))))
+
+(test-display r)
