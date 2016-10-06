f60a4b8e8f6ca468ea68218762a9b4055f33f1b1
Abraham Levkoy <alevkoy@users.noreply.github.com>
2016-10-06 17:20:22 -0400

    Complete exercises 2.14 through 2.16

--- ch2/ex2.7.scm
+++ ch2/ex2.7.scm
@@ -223,3 +223,17 @@
 (display (center e))
 (display ", percent = ")
 (display (percent e))
+
+; Exercise 2.13
+; par1 and par2 adapted from SICP.
+
+(define (par1 r1 r2)
+  (div-interval
+   (mul-interval r1 r2)
+   (add-interval r1 r2)))
+
+(define (par2 r1 r2)
+    (let ((one (make-interval 1 1)))
+        (div-interval one
+                      (add-interval (div-interval one r1)
+                                    (div-interval one r2)))))
