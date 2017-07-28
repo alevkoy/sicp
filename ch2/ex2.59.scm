577cec6581e6eebebc6b569f4b0cb730a3d69b92
Abraham Levkoy <alevkoy@users.noreply.github.com>
2017-07-27 21:24:36 -0400

    Complete exercise 2.59

---
+++ ch2/ex2.59.scm
@@ -0,0 +1,36 @@
+; Exercise 2.59
+; element-of-set?, adjoin-set, and intersection-set copied from SICP.
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2))
+         '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1)
+                                 set2)))
+        (else (intersection-set (cdr set1)
+                                set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1)
+                         (cons (car set1) set2)))))
+
+(define set1 (list 3 'a 2))
+(define set2 (list "word" 'b 3))
+
+(newline)
+(display "Union of set1 and set2: ")
+(display (union-set set1 set2))
