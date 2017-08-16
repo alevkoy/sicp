d1b2bd0e02c4728ca46918eab64a5932337ef06c
Abraham Levkoy <alevkoy@users.noreply.github.com>
2017-08-15 21:43:15 -0400

    Complete exercise 2.62

--- ch2/ex2.61.scm
+++ ch2/ex2.61.scm
@@ -51,3 +51,27 @@
 (newline)
 (display "Adjoin 1 to the empty set: ")
 (display (adjoin-set 1 '()))
+
+; Exercise 2.62
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1))
+                (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1
+                         (union-set (cdr set1) (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1
+                         (union-set (cdr set1) set2)))
+                  ((< x2 x1)
+                   (cons x2
+                         (union-set set1 (cdr set2)))))))))
+
+(define set2 (list -1 0 2 3 4 7 8 9))
+
+(newline)
+(display "Union set1 and set2: ")
+(display (union-set set1 set2))
