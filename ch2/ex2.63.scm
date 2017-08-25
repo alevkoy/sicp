84efaf3bbcff12a98ec7df77347cba9bfde44004
Abraham Levkoy <alevkoy@users.noreply.github.com>
2017-08-24 22:16:32 -0400

    Complete exercise 2.64

--- ch2/ex2.63.scm
+++ ch2/ex2.63.scm
@@ -106,3 +106,43 @@
 (display "Tree 3: ")
 (display (tree->list-2 tree3))
 ; Both routines produce (1 3 5 7 9) for all 3 trees.
+
+; Exercise 2.64
+; list->tree and partial-tree copied from SICP.
+
+(define (list->tree elements)
+  (car (partial-tree
+        elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size
+             (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree
+                elts left-size)))
+          (let ((left-tree
+                 (car left-result))
+                (non-left-elts
+                 (cdr left-result))
+                (right-size
+                 (- n (+ left-size 1))))
+            (let ((this-entry
+                   (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree
+                     (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+(newline)
+(display "(list->tree '(1 3 5 7 9 11)): ")
+(display (list->tree '(1 3 5 7 9 11)))
