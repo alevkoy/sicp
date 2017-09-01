4c49d24655e8dcb3f4afc139d9a210b8b0a80c2a
Abraham Levkoy <alevkoy@users.noreply.github.com>
2017-08-31 22:58:34 -0400

    Complete exercise 2.65

--- ch2/ex2.63.scm
+++ ch2/ex2.63.scm
@@ -146,3 +146,74 @@
 (newline)
 (display "(list->tree '(1 3 5 7 9 11)): ")
 (display (list->tree '(1 3 5 7 9 11)))
+
+; Exercise 2.65
+
+(define (union-set set1 set2)
+  (define (union-list set1 set2)
+    (cond ((null? set1) set2)
+          ((null? set2) set1)
+          (else
+            (let ((x1 (car set1))
+                  (x2 (car set2)))
+              (cond ((= x1 x2)
+                     (cons x1
+                           (union-list (cdr set1) (cdr set2))))
+                    ((< x1 x2)
+                     (cons x1
+                           (union-list (cdr set1) set2)))
+                    ((< x2 x1)
+                     (cons x2
+                           (union-list set1 (cdr set2)))))))))
+
+  (let ((set1-list (tree->list-2 set1))
+        (set2-list (tree->list-2 set2)))
+    (list->tree (union-list set1-list set2-list))))
+
+(define set1 '(5
+               (2
+                 (1 () ())
+                 (3 () ()))
+               (9
+                   (8
+                       (7 () ())
+                       ())
+                   ())))
+
+(define set2 '(1
+               ()
+               (4
+                 ()
+                 (6
+                   ()
+                   (8
+                     ()
+                     (10 () ()))))))
+
+(newline)
+(display "Union of set1 and set2: ")
+(display (union-set set1 set2))
+
+(define (intersection-set set1 set2)
+  (define (intersection-list set1 set2)
+    (if (or (null? set1) (null? set2))
+        '()
+        (let ((x1 (car set1)) (x2 (car set2)))
+          (cond ((= x1 x2)
+                 (cons x1 (intersection-list
+                            (cdr set1)
+                            (cdr set2))))
+                ((< x1 x2) (intersection-list
+                             (cdr set1)
+                             set2))
+                ((< x2 x1) (intersection-list
+                             set1
+                             (cdr set2)))))))
+
+  (let ((set1-list (tree->list-2 set1))
+        (set2-list (tree->list-2 set2)))
+    (list->tree (intersection-list set1-list set2-list))))
+
+(newline)
+(display "Intersection of set1 and set2: ")
+(display (intersection-set set1 set2))
